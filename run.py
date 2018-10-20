3ecaa54c5080e4fd8a2ccc9f9d3ae6b085c20ac7
Meelfi <40342123+Meelfi@users.noreply.github.com>
2018-10-20 15:17:16 +0200

    Update run.py

---
+++ run.py
@@ -0,0 +1,53 @@
+class Games(object):
+    pass
+# Inside Games Class
+
+
+class PC(Games):
+    pass
+
+
+class PlayStation(Games):
+    pass
+
+
+class Xbox(Games):
+    pass
+# Inside PC && Games Class
+
+
+class Rust(PC):
+    pass
+
+
+class StarCitizen(PC):
+    pass
+
+
+class Artifact(PC):
+    pass
+# Inside PlayStation && Games Class
+
+
+class Detroit(PlayStation):
+    pass
+
+
+class DaysGone(PlayStation):
+    pass
+
+
+class GodOfWar(PlayStation):
+    pass
+# Inside Xbox && Games Class
+
+class Halo(Xbox):
+    pass
+
+
+class GearsOfWar(Xbox):
+    pass
+
+
+class SeaOfThieves(Xbox):
+    pass
